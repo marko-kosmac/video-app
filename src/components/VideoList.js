485a2a5dc5e6c4ed95b0ba8de6d933b48c2c8070
Marko Kosmac <kosmac.marko@gmail.com>
2019-02-26 10:00:43 +0100

    Added styled components

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import {getVideos} from '../utils/api'
+import styled from "styled-components";
+
+const Videos = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const VideoItem = styled.div`
+  width: 200px;
+  margin: 1rem;
+`
+
+const Thumb = styled.img`
+  width: 200px;
+  height: auto;
+`
 
 class VideoList extends React.Component {
   constructor(props) {
@@ -25,22 +41,20 @@ class VideoList extends React.Component {
     const videoList = this.state.videos.length ?
       this.state.videos.map((item, idx) => {
         return item.active ? (
-          <div key={idx}>
-            {console.log('##', item)}
-            <label>
-              <h2>{item.title}</h2>
-              <p>{item.runningTime}</p>
-              <img alt={item.title} src={item.thumbUrl} />
-            </label>
-          </div>
+          <VideoItem key={idx}>
+            <h2>{item.title}</h2>
+            <p>{item.runningTime}</p>
+            <Thumb alt={item.title} src={item.thumbUrl} />
+          </VideoItem>
         ) : null
-      }) :
-      null
+      }) : null
 
     return (
       <div>
         <h1>Video list</h1>
-        {videoList}
+        <Videos>
+          {videoList}
+        </Videos>
       </div>
     )
   }
