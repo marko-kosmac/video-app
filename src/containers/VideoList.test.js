72e1b54cf69a781eea6f01791acde02d5fc3381f
Marko Kosmac <kosmac.marko@gmail.com>
2019-02-27 07:56:08 +0100

    Added some tests

---
+++ src/containers/VideoList.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VideoList from './VideoList';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<VideoList />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
